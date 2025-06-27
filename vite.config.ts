8200a0d869ca11abb10cafacd1c492d12ebe94a2
wfceza <wfceza@gitgub.io>
2025-06-27 12:29:36 +0100

    d

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      base: '/vite-deploy-demo/'
+      base: '/social-media/'
     },
   },
 }));
